882b27e482200ae5a80af9f7c6fac80d911dc87c
kalesha85 <kaleshab@propelapps.com>
2024-06-29 16:58:26 +0530

    29-06-2024-Intro-screen

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -7,7 +7,11 @@ export default function RootLayout() {
 
   return (
       <Stack>
-        <Stack.Screen name="index" />
+        <Stack.Screen name="index"
+        options={{
+          headerShown:false
+        }}
+        />
       </Stack>
 
   );
--- app/index.tsx
+++ app/index.tsx
@@ -1,10 +1,11 @@
 import { StyleSheet, Text, View } from 'react-native'
 import React from 'react'
+import AnimatedIntro from '@/components/AnimatedIntro'
 
 export default function index() {
   return (
     <View>
-      <Text>index kk</Text>
+     <AnimatedIntro/>
     </View>
   )
 }
